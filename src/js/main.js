1ad02e87433be3551786b107e2d87d0173f63989
Alya S <Alya.SerraFolly@studentfr.ch>
2023-11-13 00:29:51 +0100

    Header - main WIP

--- src/js/main.js
+++ src/js/main.js
@@ -1,48 +1,73 @@
 import { gsap, Power2 } from "gsap";
 
-gsap.from(
-  ".landing-text",
-  { opacity: 0, y: 0 },
-  {
-    opacity: 1,
-    y: 0,
-    duration: 0.5,
+document.addEventListener("DOMContentLoaded", function () {
+  // Animation landing text - Apparition fixe
+  gsap.from(".landing-text", {
+    opacity: 0,
+    duration: 0, // Pas de durée pour une apparition instantanée
     ease: Power2.easeInOut,
     onStart: () => {
       document.querySelector(".landing-text").style.display = "block";
     },
+  });
+
+  // Animation landing text - Disparition de bas en haut
+  gsap.to(".landing-text", {
+    opacity: 0,
+    y: "-100%", // Disparaître vers le haut
+    duration: 1.2, // Durée de l'ease-out
+    ease: Power2.easeInOut,
+    delay: 2,
     onComplete: () => {
-      gsap.to(".landing-text", {
-        opacity: 0,
-        duration: 1.7,
-        ease: Power2.easeInOut,
-      });
+      document.querySelector(".landing-text").style.display = "none";
     },
+  });
+
+  gsap.to(".landing-text", {
+    opacity: 1,
+    duration: 1,
+    ease: Power2.easeInOut,
+    delay: 3,
+  });
+
+  // MenuBurger
+  const element = document.querySelector(".menu-btn-6");
+
+  function menuBtnFunction(menuBtn) {
+    menuBtn.classList.toggle("active");
   }
-);
 
-gsap.to(".landing-hidden", {
-  y: "-100%",
-  duration: 1,
-  ease: Power2.easeInOut,
-  delay: 2.2,
-  onStart: () => {
-    landingVisible = true;
-  },
-  onComplete: () => {
-    document.querySelector(".landing-hidden").style.display = "none";
-    document.body.style.overflow = "auto";
-    landingVisible = false;
-  },
-});
+  if (element) {
+    element.addEventListener("click", function () {
+      menuBtnFunction(this);
+    });
+  }
 
-// MenuBurger
-const element = document.querySelector(".menu-btn-6");
+  const menu = document.querySelector(".menu");
+  const overlay = document.querySelector(".menu-overlay");
 
-function menuBtnFunction(menuBtn) {
-  menuBtn.classList.toggle("active");
-}
+  const openAnimation = gsap.to(overlay, {
+    duration: 0.5,
+    height: "100%",
+    ease: "power2.inOut",
+    paused: true,
+  });
 
-element.addEventListener("click", function () {
-  menuBtnFunction(this);
+  gsap.from(".menu-overlay", {
+    opacity: 0,
+    y: -100,
+    height: "100%",
+    duration: 2.5,
+    ease: "power2.inOut",
+  });
+
+  menu.addEventListener("click", () => {
+    const isOpen = menu.classList.toggle("open");
+    overlay.classList.toggle("open");
+
+    if (isOpen) {
+      openAnimation.play();
+    } else {
+    }
+  });
 });
