ea07cb800412ba39b3edfed8bfa877810e8bb820
Alya S <Alya.SerraFolly@studentfr.ch>
2023-11-12 14:04:44 +0100

    Working on navigation

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,31 @@
-import { gsap } from "gsap";
+import { gsap, Power2 } from "gsap";
+
+gsap.from(
+  ".landing-text",
+  { opacity: 0, y: 0 },
+  {
+    opacity: 1,
+    y: 0,
+    duration: 0.5,
+    ease: Power2.easeInOut,
+    onStart: () => {
+      document.querySelector(".landing-text").style.display = "block";
+    },
+    onComplete: () => {
+      gsap.to(".landing-text", {
+        opacity: 0,
+        duration: 1.7,
+        ease: Power2.easeInOut,
+      });
+    },
+  }
+);
 
 gsap.to(".landing-hidden", {
-  opacity: 0,
-  duration: 1.7,
-  ease: "power2.inOut",
+  y: "-100%",
+  duration: 1,
+  ease: Power2.easeInOut,
+  delay: 2.2,
   onStart: () => {
     landingVisible = true;
   },
@@ -12,4 +34,15 @@ gsap.to(".landing-hidden", {
     document.body.style.overflow = "auto";
     landingVisible = false;
   },
+});
+
+// MenuBurger
+const element = document.querySelector(".menu-btn-6");
+
+function menuBtnFunction(menuBtn) {
+  menuBtn.classList.toggle("active");
+}
+
+element.addEventListener("click", function () {
+  menuBtnFunction(this);
 });
