1577f3b53edb015818718e946b098e91ed0815dd
Alya S <Alya.SerraFolly@studentfr.ch>
2023-11-13 11:51:59 +0100

    Working on section-2

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,6 @@
 import { gsap, Power2 } from "gsap";
+import "sortable-tablesort/sortable.min.css";
+import "sortable-tablesort/sortable.min.js";
 
 document.addEventListener("DOMContentLoaded", function () {
   // Animation landing text - Apparition fixe
