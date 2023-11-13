a63d160a8d6020969b63151d9385c34a9a14f449
Alya S <Alya.SerraFolly@studentfr.ch>
2023-11-13 02:45:41 +0100

    WIP

--- src/js/main.js
+++ src/js/main.js
@@ -71,3 +71,30 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+const $cursor = document.querySelector(".cursor__circle");
+const $hover = document.querySelectorAll("a");
+
+document.body.addEventListener("mousemove", onMouseMove);
+for (let i = 0; i < $hover.length; i++) {
+  $hover[i].addEventListener("mouseenter", onMouseHover);
+  $hover[i].addEventListener("mouseleave", onMouseHoverOut);
+}
+
+function onMouseMove(e) {
+  TweenMax.to($cursor, 0.4, {
+    x: e.pageX - 16,
+    y: e.pageY - 16,
+  });
+}
+
+function onMouseHover() {
+  TweenMax.to($cursor, 0.4, {
+    scale: 3,
+  });
+}
+function onMouseHoverOut() {
+  TweenMax.to($cursor, 0.4, {
+    scale: 1,
+  });
+}
